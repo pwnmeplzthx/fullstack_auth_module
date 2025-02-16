0c51bc59c7886cc50ceaa1acc765ba4429214e3d
pwnmeplzthx <pshumakov11@mail.ru>
2025-02-16 17:07:25 +0300

    add utils functions, setting up session management

--- auth-server/src/main.ts
+++ auth-server/src/main.ts
@@ -3,17 +3,49 @@ import { ConfigService } from '@nestjs/config';
 
 import * as cookieParser from 'cookie-parser';
 
+import IORedis from 'ioredis';
+
+import * as session from 'express-session';
+
+import { RedisStore } from 'connect-redis';
+
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import msFn from './libs/common/utils/ms-util';
+import getBoolean from './libs/common/utils/getBoolean';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const config = app.get(ConfigService);
+    const redis = new IORedis(config.getOrThrow<string>('REDIS_URI'));
+
     app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
+        }),
+    );
+
+    app.use(
+        session({
+            secret: config.getOrThrow<string>('SESSION_SECRET'),
+            name: config.getOrThrow<string>('SESSION_NAME'),
+            resave: true,
+            saveUninitialized: false,
+            cookie: {
+                domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+                maxAge: msFn(config.getOrThrow<string>('SESSION_MAX_AGE')),
+                httpOnly: getBoolean(
+                    config.getOrThrow<string>('SESSION_HTTP_ONLY'),
+                ),
+                secure: getBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+                sameSite: 'lax',
+            },
+            store: new RedisStore({
+                client: redis,
+                prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+            }),
         }),
     );
 
